ba946cea5cfca57af550bc2868898b9127296062
Kiriko-I <124024707+Kiriko-I@users.noreply.github.com>
2023-05-06 09:13:45 +0000

    [Add]devise関連のルーティング

--- db/migrate/20230506044429_devise_create_users.rb
+++ db/migrate/20230506044429_devise_create_users.rb
@@ -31,7 +31,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-
+      
+      # 追加カラム
+      t.string :name, null: false
+      t.string :nickname, null: false
+      t.integer :prefecture, null: false, default: 0
 
       t.timestamps null: false
     end
