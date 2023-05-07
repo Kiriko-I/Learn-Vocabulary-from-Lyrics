d13f8b3c1e284568b11d15bd78734d3680637fdb
Kiriko-I <124024707+Kiriko-I@users.noreply.github.com>
2023-05-07 07:09:12 +0000

    [Add]routing #3

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,13 @@ Rails.application.routes.draw do
   }
   devise_for :users, skip: [:passwords], controllers: {
     registrations: "user/registrations",
-    sessions: 'public/sessions'
+    sessions: 'user/sessions'
   }
+  # ユーザーログイン画面をルートパスにする
+  # scope module: :users do
+  #   root 'sessions#new'
+  # end
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
