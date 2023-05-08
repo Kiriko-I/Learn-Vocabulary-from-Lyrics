4ae7ebf7b3f5f1dbf5cde65b98827e76561b50a3
Kiriko-I <124024707+Kiriko-I@users.noreply.github.com>
2023-05-08 08:14:21 +0000

    [Add]header, user's log out #5

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -21,10 +21,10 @@ class User::SessionsController < Devise::SessionsController
   # protected
   
   def after_sign_in_path_for(resource)
-    users_root_path
+    posts_path
   end
   
-  def after_sign_out_path_foru(resource)
+  def after_sign_out_path_for(resource)
     new_user_session_path
   end
 
