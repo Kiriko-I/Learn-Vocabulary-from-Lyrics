3e6d6c72659fdc62644d6650bb5b935df70228a7
Kiriko-I <124024707+Kiriko-I@users.noreply.github.com>
2023-05-09 09:51:20 +0000

    [Add]admin sign-up, posts' index layout

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,2 +1,18 @@
 class Admin::PostsController < ApplicationController
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+  
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
 end
