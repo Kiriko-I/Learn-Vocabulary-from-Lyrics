e6ba565adfa847d7f1e77b5a80ce1e3aeeabd9d4
Kiriko-I <124024707+Kiriko-I@users.noreply.github.com>
2023-05-10 08:57:36 +0000

    [Add]admin user index/show

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,44 @@
 class Admin::UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      redirect_to user_path(@user), notice: "You have updated user successfully."
+    else
+      render "edit"
+    end
+  end
+  
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to new_user_session_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :nickname, :prefecture, :profile_image)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+
 end
--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -1,2 +1,19 @@
 class User::UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to admin_users_path
+  end
+
 end
