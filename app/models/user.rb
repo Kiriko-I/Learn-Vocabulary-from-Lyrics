fddfeef60dda06bfad82581287aa6c898506ce75
Kiriko-I <124024707+Kiriko-I@users.noreply.github.com>
2023-05-23 08:55:34 +0000

    [Add]ゲストユーザができることの範囲、レイアウトの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,7 +54,7 @@ class User < ApplicationRecord
   end
 
   def self.guest
-    find_or_create_by!(name: 'guestuser', nickname: 'guest', email: 'guest@example.com') do |user|
+    find_or_create_by!(name: 'guestuser', nickname: 'ゲスト', email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
       user.name = "guestuser"
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,19 @@ Admin.create!(
   email: 'admin@example.com',
   password: 'password'
 )
+
+users = User.create!(
+  [
+    {email: 'a@sample.com', password: 'password', name: '加瀬 早織', nickname: '加瀬 早織', prefecture: 12, profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.png"), filename:"sample-user1.png")},
+    {email: 'b@sample.com', password: 'password', name: '西田 直人', nickname: '西田 直人', prefecture: 14, profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.png"), filename:"sample-user2.png")},
+    {email: 'c@sample.com', password: 'password', name: '比嘉 めぐみ', nickname: '比嘉 めぐみ', prefecture: 1, profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.png"), filename:"sample-user3.png")},
+  ]
+)
+
+Post.create!(
+  [
+    {prefecture: 12, city: '千葉市中央区', landmark: '中央区役所前バス停', sidewalk: 3, snow_height: 3, snow_state: 2, message: '歩きにくいですが、今日も仕事頑張ります', latitude: 35.609001, longitude: 140.124588, snow_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.jpg"), filename:"sample-post1.jpg"), user_id: users[0].id},
+    {prefecture: 14, city: '神奈川県横浜市', landmark: '石川町駅', sidewalk: 4, snow_height: 2, snow_state: 1, message: '結構ふぶいてます', latitude: 35.439231, longitude: 139.642468, snow_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post2.jpg"), filename:"sample-post2.jpg"), user_id: users[1].id},
+    {prefecture: 1, city: '美瑛町', sidewalk: 4, snow_height: 4, snow_state: 1, message: '歩く場所ありません…', latitude: 43.588280, longitude: 142.467056, snow_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post3.jpg"), filename:"sample-post3.jpg"), user_id: users[2].id},
+  ]
+)
