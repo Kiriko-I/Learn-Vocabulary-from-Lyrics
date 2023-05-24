c0ccd405b6c03eacc18feb2001653041f7edf9e5
Kiriko-I <124024707+Kiriko-I@users.noreply.github.com>
2023-05-24 05:35:15 +0000

    [Add]GETメソッドに関係するコントローラにバリデーションを追加

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,5 +1,5 @@
 class Admin::SearchesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_admin_user!
 
 	def search
 	  @model = params[:model]
@@ -12,5 +12,5 @@ class Admin::SearchesController < ApplicationController
 		  @users = User.search_for(@nickname).order(created_at: :desc)
 		end
 	end
-	
+
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,5 @@
 class Admin::UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_admin_user!
 
   def show
     @user = User.find(params[:id])
