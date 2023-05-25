efee1dabc326ee01432c4d298e912c15fa213338
Kiriko-I <124024707+Kiriko-I@users.noreply.github.com>
2023-05-25 01:43:05 +0000

    finish

--- app/controllers/user/comments_controller.rb
+++ app/controllers/user/comments_controller.rb
@@ -3,9 +3,6 @@ class User::CommentsController < ApplicationController
   def create
     post = Post.find(params[:post_id])
     @comment = current_user.comments.new(comment_params)
-    # ↑下記の記述をまとめたもの
-    # @comment = Comment.new(comment_params)
-    # @comment.user_id = current_user.id    
     @comment.post_id = post.id
     if @comment.save
       flash[:success] = 'コメントを投稿しました。'
@@ -25,7 +22,7 @@ class User::CommentsController < ApplicationController
   end
 
   private
-  
+
   def comment_params
     params.require(:comment).permit(:comment)
   end
