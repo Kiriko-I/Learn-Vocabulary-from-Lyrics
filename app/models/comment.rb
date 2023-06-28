a27d6d2c144ca246fbc9f304a0736fef4933b670
Kiriko-I <124024707+Kiriko-I@users.noreply.github.com>
2023-06-28 10:58:12 +0000

    [Add]コメントのバリデーションを強化

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,11 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
-  validates :comment, presence: true, length: { maximum: 70 }
+  validates :comment, presence: true, length: { maximum: 70 }, if: :validate_no_spaces_or_line_breaks
+  
+  # 改行のみ・全角半角スペースのみのメッセージか確認する
+  def validate_no_spaces_or_line_breaks
+    comment.strip.blank? 
+  end
+
 end
